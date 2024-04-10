bd0962666e274b2fde589efb7b6efabb7ed11f39
Devapriya L <devapriyakodinjoor24@gmail.com>
2024-04-10 16:05:52 +0530

    feat: navigation to cardList screen

--- src/screens/AppScreens/Contacts.tsx
+++ src/screens/AppScreens/Contacts.tsx
@@ -213,6 +213,13 @@ const ContactsPage = () => {
     }
   };
 
+const cardListPage = (id: string, name: string) => {
+  navigation.navigate('CardStack', {
+    screen: 'CardListScreen',
+    params: { card_id: id, name: name },
+  });
+}
+
   const goToSearchScreen = () => {
     navigation.navigate('SearchScreen');
   };
@@ -237,6 +244,7 @@ const ContactsPage = () => {
             <ContactListComponent
               contactName={item.contact_name}
               onPress={() => contactPage(item.card_id, item.contact_name)}
+              onLongPress={() => cardListPage(item.card_id, item.contact_name)}
             />
           )}
           keyExtractor={(item) => item.card_id + item.contact_name}
